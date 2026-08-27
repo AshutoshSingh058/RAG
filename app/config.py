935906f35c56b2a181b3c3eebbba4f12e26d98e1
AshutoshSingh058 <singhash058@gmail.com>
2026-08-28 01:00:44 +0530

    Build retrieval, added test data

--- app/config.py
+++ app/config.py
@@ -5,9 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    GEMENI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
     QDRANT_URL = os.getenv("QDRANT_CLUSTER_ENDPOINT")
     QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_COLLECTION = "enterprise_rag"
 
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     GROQ_MODEL = "llama-3.3-70b-versatile"
